e90440b4d337e6be46605324e9159aa5fecea9d0
Tharanga Weerappullige <tharangasadamali94@gmail.com>
2019-03-26 22:38:19 +0530

    adding icons

--- development/resources/views/partials/header.blade.php
+++ development/resources/views/partials/header.blade.php
@@ -9,17 +9,16 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="#">Shopping Cart</a>
+        <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i>Shopping Cart</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user"></i>
           User Account
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">User Account</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Logout</a>
         </div>
       </li>
     </ul>
