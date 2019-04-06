2874d557fe1da75977ff3e9b41fec00ff2704e92
Tharanga Weerappullige <tharangasadamali94@gmail.com>
2019-04-06 10:10:08 +0530

    database error complete

--- development/resources/views/shop/index.blade.php
+++ development/resources/views/shop/index.blade.php
@@ -4,5 +4,16 @@
 	Laravel Shopping Cart
 @endsection
 @section('content')
-	<h1>It works</h1>
+	<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="..." alt="...">
+      <div class="caption">
+        <h3>Thumbnail label</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
